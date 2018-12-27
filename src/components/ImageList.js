21e41964f45d5c3ffda501d86fd4e0c25fd0b2e1
“Kevin <logikevcoder@gmail.com>
2018-12-27 14:14:33 -0600

    added imageRefs and stlying

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import ImageCard from "./ImageCard";
+import './ImageList.css';
+
 
 const ImageList = (props) => {
-  const imageList = props.images.map(({description, id, urls}) => {
-    return <img key={id} src={urls.regular} alt={description} />
+  const imageList = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />
   });
 
   return (
-    <div>
+    <div className='image-list'>
       {imageList}
     </div>
   )
